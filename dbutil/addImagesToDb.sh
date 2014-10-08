668678250cc409d35d87f55165f296f4267582ba
Alan <alancharlescramer@gmail.com>
2014-10-08 14:09:33 -0400

    add images to database as attachments

--- dbutil/addImagesToDb.sh
+++ dbutil/addImagesToDb.sh
@@ -5,13 +5,19 @@
 #
 #
 i=0
+curlcmd="curl -X PUT http://127.0.0.1:5984/rop_images/"
 for arg in $*
 do
    i=$((i+1))
    filename=$(cd $(dirname "$arg"); pwd)/$(basename "$arg")
    echo "adding $i : $filename"
    curldata="{\"origin\":\"$filename\"}"
-   cmd="curl -X PUT http://127.0.0.1:5984/rop_images/$i -d $curldata"
+   cmd="$curlcmd$i -d $curldata"
    echo $cmd
    res=$($cmd) 
+   echo $res
+   IFS='"' read -a array <<< "$res"
+   rev=${array[9]}
+   cmd="$curlcmd$i/$(basename "$arg")?rev=$rev --data-binary @$arg -H \"Content-Type: image/jpg\""
+   res=$($cmd)
 done
