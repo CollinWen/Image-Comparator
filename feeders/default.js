f0ce558e35237e7616d4697809017e4e14d2cbc8
Alan <alancharlescramer@gmail.com>
2014-10-08 14:41:54 -0400

    feeder use couchdb to get images, style hover in css

--- feeders/default.js
+++ feeders/default.js
@@ -9,13 +9,13 @@ var ImageCompare = (function (IC) {
         // load two image from image db
         var img0 = document.getElementById("image0");
 
-        img0.src = "../images/normal_retina.jpg";
-         img0.style.width = "700px";
+        img0.src = "http://127.0.0.1:5984/rop_images/3/retina1.jpg";
+        img0.style.width = "700px";
         img0.style.height = "400px";
         
         var img1 = document.getElementById("image1");
 
-        img1.src = "../images/retina1.jpg";    
+        img1.src = "http://127.0.0.1:5984/rop_images/1/AMD-Retina.jpg"; 
         img1.style.width = "700px";
         img1.style.height = "400px";        
     };
