f8e0ae2650262782f51886bc7bb0e8edb72c3877
Alan <alancharlescramer@gmail.com>
2014-10-26 19:40:20 -0400

    added configurator that creates image database and adds views

---
+++ dbutil/configurator.js
@@ -0,0 +1,111 @@
+
+
+$(document).ready(function() {
+    document.getElementById('openimage').addEventListener('change', handleFileSelect, false);
+});
+
+handleFileSelect = function (evt) {
+
+    var files = evt.target.files; // FileList object
+    var file = files[0];
+        
+    for (var i = 0 ; i < files.length; ++i) {
+        var fileReadAsDataUrl = new FileReader();
+        fileReadAsDataUrl.onload = (function(progEvt) {
+
+            var imageAsDataUrl = progEvt.target.result;
+            
+            var img = new Image();            
+            img.src = progEvt.target.result;
+
+            document.getElementById('imgdisp').src = img.src;
+            
+            addImageToImageDb(img);
+        });
+        fileReadAsDataUrl.readAsDataURL(files[i]);   
+    }    
+};
+
+// from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+//
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+var dburl =  'http://127.0.0.1:5984/rop_images_1/';
+
+
+addImageToImageDb = function(img) {
+
+    var dataStr = "{\"origin\":" + "\"configurator\"}";
+
+    $.ajax({
+        url : dburl+generateUUID(),
+        type : 'PUT',
+        data: dataStr,
+        success : function(json) { 
+            console.log ("put succeeded: " + JSON.stringify(json)); 
+        },
+        error: function (response) {
+            console.log("put failed : " + JSON.stringify(response));
+        }
+    });
+};
+
+onCreateImageDb = function() {
+
+    $.ajax({
+        url : dburl,
+        type : 'PUT',
+        //data: dataStr,
+        success : function(json) { 
+            console.log ("put succeeded: " + JSON.stringify(json)); 
+        },
+        error: function (response) {
+            console.log("put failed : " + JSON.stringify(response));
+        }
+    });
+    
+    // add design doc that loads basic views
+    var des_json = 
+       " {\"_id\":\"_design/basic_views\", " +
+       "  \"views\": {" +
+       "     \"all_docs\" :  {" +
+       "            \"map\" : \"function(doc) {" +
+       "               emit(null, doc);" +
+       "            }\" "+
+       "        }," +
+       "        \"count_docs\" : {                                   " +
+       "            \"map\" : \"function(doc) {                      " +
+       "                emit(null, doc);                             " +
+       "            }\",                                                " +
+       "            \"reduce\" : \"_count(keys, vals, rereduce)\"    " +
+       "        }                                                    " +
+       "    }" +
+       "}";
+ 
+    var des_url = dburl + "_design/basic_views";
+    $.ajax({
+        url : des_url,
+        type : 'PUT',
+        data: des_json,
+        success : function(json) { 
+            console.log ("put succeeded: " + JSON.stringify(json)); 
+        },
+        error: function (response) {
+            console.log("put failed : " + JSON.stringify(response));
+        }
+    });
+};
+
+onLoadImages = function() {
+
+    document.getElementById('openimage').click();
+};
+
