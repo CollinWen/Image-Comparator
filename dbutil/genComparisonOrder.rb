a6bb5edca298d6989791977fee5b8a4c7a3bf5c4
Alan <alancharlescramer@gmail.com>
2015-04-05 19:58:35 -0400

    generate list of comparisons, 20% dups, and post to rop_images

--- dbutil/genComparisonOrder.rb
+++ dbutil/genComparisonOrder.rb
@@ -2,20 +2,60 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+# get the number of documents as a command line arg
 ARGV.each { |arg| puts "Argument: #{arg}" }
-
+# todo - error checking on inputs
 numDocs = ARGV[0].to_i
-res = (Array (1..numDocs)).shuffle
+puts numDocs.inspect
 
+if (numDocs < 2) then 
+    puts "Must specify the number of documents to consider. ";
+    puts "For example: ruby genComparisonOrder.rb 6 ";
+    exit
+end
+
+# create pairs
+pairs = []
+for i in (1..numDocs) do 
+    for j in (i+1..numDocs) do
+        pairs.push([i, j])
+    end
+end 
+#puts pairs.inspect
+#puts pairs.size
+
+# shuffle them 
+pairs.shuffle!
+#puts pairs.inspect
+#puts pairs.size
+
+# add 20% duplicates by finding a random entry and inserting it randomly
+dupCt = pairs.size/5 +1
+puts "dup count is #{dupCt}."
+for i in (1..dupCt) do
+    idx = rand(pairs.size) # who to repeat
+    #puts "repeating #{idx}"
+    pair = pairs[idx].dup # duplicate the array
+    pair.reverse! # if the repeat shows up next to the original, this will be good
+    #puts "pair is " + pair.inspect
+    idx = rand(pairs.size) # where to put the repeat
+    #puts "pair is going to #{idx}"
+    pairs.insert(idx, pair)
+end
+puts pairs.inspect
+puts pairs.size
+
+
+# put the results in the database
 dbname = "rop_images/"
-docname = "comparison_ordering"
+docname = "task1"
 url = 'http://localhost:5984/' + dbname + docname
 uri = URI.parse(url)
  
 http = Net::HTTP.new(uri.host, uri.port)
 request = Net::HTTP::Put.new(uri.path)
 
-resp = http.request(request, "{\"idx order\": \"#{res}\"}") 
+resp = http.request(request, "{\"type\" : \"task\", \"count\" : \"#{pairs.size}\", \"idx order\": \"#{pairs}\"}") 
 puts resp.body
 
 
