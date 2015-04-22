b9e12b48ee77bfb8bd5d75373c20fe748440254e
kalpathy <kalpathy@nmr.mgh.harvard.edu>
2015-04-21 21:39:00 -0400

    headway on status results

---
+++ results/status.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+  displayStatus();
+});
+
+
+sortResults = function(resultArray) {
+
+  // first build hash, one entry per image, counting #times shown
+  var numTimesShown = {};
+  for (var ires = 0 ; ires < resultArray.length; ++ires) {
+    var res = resultArray[ires].value;
+
+    if (!numTimesShown[res.image0])
+      numTimesShown[res.image0] = 0;
+
+    if (!numTimesShown[res.image1])
+      numTimesShown[res.image1] = 0;
+
+    numTimesShown[res.image0]++;
+    numTimesShown[res.image1]++;
+  }
+
+  // next build hash, one entry per image, counting #times winner
+  var numTimesWon = {};
+  for (var ires = 0 ; ires < resultArray.length; ++ires) {
+    var res = resultArray[ires].value;
+
+    if (!numTimesWon[res.image0])
+        numTimesWon[res.image0] = 0;
+
+    if (!numTimesWon[res.image1])
+        numTimesWon[res.image1] = 0;
+
+    if (res.winner === "0")
+       numTimesWon[res.image0]++;
+
+    if (res.winner === "1")
+        numTimesWon[res.image1]++;
+  }
+
+  var images = Object.keys(numTimesShown);
+  var sortedImages = images.sort(function(a, b) {
+    return numTimesWon[a]/numTimesShown[a] < numTimesWon[b]/numTimesShown[b];
+  });
+
+  return sortedImages;
+
+};
+
+// add a bunch of images
+displayResults=function(resArray){
+
+
+  resArray.forEach(function(res){
+    var img = new Image();
+    var div = document.getElementById('image-container');
+
+    img.onload = function() {
+      div.appendChild(img);
+    };
+
+    img.src = res + "/image";
+  });
+}
+
+
+displayStatus=function(){
+
+  var hostname="http://localhost:5984/";
+  var imageDbName = "rop_images/";
+  var resultsDbName = "image_compare_results/";
+
+  var fullurl = hostname + resultsDbName + '_design/basic_views/_view/taskresults';
+  $.ajax({
+    url : fullurl,
+    type : 'GET',
+    success : function(json) {
+      console.log(json);
+      var results = jQuery.parseJSON( json );
+
+      var mikeResults = results.rows.filter(function(result){
+        return result.value.user === 'susan'; });
+
+      var mikeSortedRes = sortResults(mikeResults);
+
+      displayResults(mikeSortedRes);
+
+      var rc=document.getElementById("rowCt");
+      rc.textContent=results.total_rows;
+    },
+    error: function (response) {
+      console.log("get failed : " + JSON.stringify(response));
+    }
+  });
+}
--- ui/two_image.js
+++ ui/two_image.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
 
     var user = $("#username").val();
     ImageCompare.TaskFeeder.SetImagePair(user);
-    
+
 });
 
-// labels can be either primary or danger 
+// labels can be either primary or danger
 // just for controlling the color through bootstrap
 setLabelDanger = function(isDanger, label) {
 
@@ -31,7 +31,7 @@ updateStatusInfo = function() {
     var label = $("#si_user_label");
     var isDanger = (selUser.value === "testuser");
     setLabelDanger(isDanger, label);
-   
+
     // update database
     var elem = document.getElementById("si_db");
     var db_elem = document.getElementById("database");
@@ -40,10 +40,10 @@ updateStatusInfo = function() {
     var label = $("#si_db_label");
     var isDanger = (seldb.value === "localhost");
     setLabelDanger(isDanger, label);
-    
+
     // update tasks
     getTasks(selUser.value, updateStatInfoTasks);
-    
+
 };
 
 // called on getTasks success, input are the rows from the view
@@ -51,23 +51,23 @@ updateStatusInfo = function() {
 updateStatInfoTasks = function(json) {
     var result = jQuery.parseJSON( json );
     var tasks = result.rows;
-    
+
     var elem = document.getElementById("si_tasks");
     elem.textContent = "You have " + tasks.length + " unfinished tasks.";
-   
-    // this is to be updated - hide it if there are no pending tasks 
-    var curTaskElem = document.getElementById("si_curtask");   
-   
+
+    // this is to be updated - hide it if there are no pending tasks
+    var curTaskElem = document.getElementById("si_curtask");
+
     if (tasks.length > 0) {
         curTaskElem.hidden = false;
-    
+
         var firstTask = tasks[0].value;
         var icl_id = firstTask.image_compare_list;
 
         var dburl = ImageCompare.TaskFeeder.GetImageDbUrl();
-        var fullurl = dburl + "_design/basic_views/_view/image_compare_lists?key=\"" + icl_id + "\"";  
-        
-        $.ajax({  
+        var fullurl = dburl + "_design/basic_views/_view/image_compare_lists?key=\"" + icl_id + "\"";
+
+        $.ajax({
             url : fullurl,
             type : 'GET',
             success : function (json) {
@@ -76,7 +76,7 @@ updateStatInfoTasks = function(json) {
 
                 var curIdx = firstTask.current_idx + 1; // because humans usually don't use zero based indexing
                 curTaskElem.textContent = "You are on comparison " + curIdx + " of " + result.rows[0].value.count;
-                
+
             },
             error: function (response) {
                 console.log("get failed : " + JSON.stringify(response));
@@ -93,8 +93,8 @@ var getTasks = function(username, successFn) {
 
     var dburl = ImageCompare.TaskFeeder.GetImageDbUrl();
     var fullurl = dburl + "_design/basic_views/_view/incomplete_tasks?key=\"" + username + "\"";
-    
-    $.ajax({  
+
+    $.ajax({
         url : fullurl,
         type : 'GET',
         success : successFn,
@@ -104,10 +104,10 @@ var getTasks = function(username, successFn) {
     });
 }
 
-// THIS IS A DB PUT 
+// THIS IS A DB PUT
 // winVal is a number representing how much A is greater than B
 // In a situation where the user can pick one or the other or neither
-// winVal will be -1, 0, or 1. This can support other values for UIs 
+// winVal will be -1, 0, or 1. This can support other values for UIs
 // where the user can say "A five times more than B"
 // todo - this should not be global
 createICResult = function(winVal, img0, img1, user, comment, task, task_idx) {
@@ -116,29 +116,29 @@ createICResult = function(winVal, img0, img1, user, comment, task, task_idx) {
     var db_config_elem = document.getElementById("database");
     var db_config = db_config_elem.options[db_config_elem.selectedIndex].value;
     var hostname = db_config === "localhost" ?
-        "http://localhost:5984/" : 
+        "http://localhost:5984/" :
         "http://ec2-54-152-40-100.compute-1.amazonaws.com:5984/";
     var imageDbName = "rop_images/";
     var resultsDbName = "image_compare_results/";
-    
+
     var currentTime = new Date();
     var timeStr = currentTime.toString();
     var imgDbStr = hostname + imageDbName;
-    
+
     var dataStr = "{\"user\":\"" + user + "\",";
     dataStr += "\"date\":\"" + timeStr + "\",";
     dataStr += "\"image0\":\"" + imgDbStr + img0.toString() + "\",";
     dataStr += "\"image1\":\"" + imgDbStr + img1.toString() + "\",";
     dataStr += "\"winner\":\"" +  winVal.toString() + "\",";
-    
+
     if (comment != ImageCompare.TaskFeeder.defaultComment) {
         dataStr += ",";
         dataStr += "\"comment\":\"" + comment + "\",";
     }
-    
-    dataStr += "\"task\":\"" +  task + "\",";
+
+    dataStr += "\"task\":\"" +  task._id + "\",";
     dataStr += "\"task_idx\":\"" +  task_idx + "\"";
-    
+
     dataStr += "}";
 
     var def = $.ajax({
@@ -146,53 +146,53 @@ createICResult = function(winVal, img0, img1, user, comment, task, task_idx) {
         type : 'PUT',
         //dataType : "jsonp",
         data: dataStr,
-        success : function(json) { 
-            console.log ("put succeeded: " + JSON.stringify(json)); 
+        success : function(json) {
+            console.log ("put succeeded: " + JSON.stringify(json));
         },
         error: function (response) {
             console.log("put failed : " + JSON.stringify(response));
         }
     });
-    
+
     return def;
 };
 
-// THIS IS A DB PUT 
+// THIS IS A DB PUT
 // increments the task's current idx and posts it back to the database
-// user is used to set the next image pair 
+// user is used to set the next image pair
 // todo - this should not be global
 updateTask = function(task, user) {
 
     // first get the length of the icl for the task, (to see if the task is now complete)
     var dburl = ImageCompare.TaskFeeder.GetImageDbUrl();
-    var fullurl = dburl + "_design/basic_views/_view/icl_lengths?key=\"" + task.image_compare_list + "\""; 
+    var fullurl = dburl + "_design/basic_views/_view/icl_lengths?key=\"" + task.image_compare_list + "\"";
     var icl_count = -1;
-    
-    
+
+
     var defered = $.ajax({
         url : fullurl,
         type : 'GET',
-        success : function(json) { 
-                                            
+        success : function(json) {
+
             var result = jQuery.parseJSON( json );
             icl_count = result.rows[0].value;
-            
+
             // now that that worked, update the task
             var dburl = ImageCompare.TaskFeeder.GetImageDbUrl();
-            var fullurl = dburl + task._id;    
-            
+            var fullurl = dburl + task._id;
+
             task.current_idx++;
             if (task.current_idx >= icl_count) {
                 task.completed = true;
             }
-            
+
             $.ajax({
                 url : fullurl,
                 type : 'PUT',
                 data: JSON.stringify(task),
                 contentType: "application/json",
-                success : function(json) { 
-                    console.log ("put succeeded: " + JSON.stringify(json)); 
+                success : function(json) {
+                    console.log ("put succeeded: " + JSON.stringify(json));
                     ImageCompare.TaskFeeder.SetImagePair(user);
                     updateStatusInfo(); // really this is redundant, but I need to return a deferred for this ajax call - how?
                 },
@@ -204,7 +204,7 @@ updateTask = function(task, user) {
         error: function (response) {
             console.log("put failed : " + JSON.stringify(response));
         }
-    });    
+    });
 
     return defered;
 };
@@ -226,13 +226,12 @@ OnSetUser = function(sel) {
 saveResultSetImages = function (winnerId) {
     var img0 = ImageCompare.TaskFeeder.Image0;
     var img1 = ImageCompare.TaskFeeder.Image1;
-    var task = ImageCompare.TaskFeeder.current_task; 
     var task_idx = ImageCompare.TaskFeeder.current_task_idx;
     var task = ImageCompare.TaskFeeder.current_task;
-    
+
     var comment = $("#compare-comment").val();
     var user = $("#username").val();
-    
+
     // these two are like a transaction - how to ensure both or neither?
     var d1 = createICResult(1, img0, img1, user, comment, task, task_idx);
     var d2 = updateTask(task, user);
@@ -240,8 +239,8 @@ saveResultSetImages = function (winnerId) {
     // ImageCompare.TaskFeeder.SetImagePair(user);
     // instead it has to happen inside the updateTask success
     // Todo: maybe better would be to pass it in.
-    
-    // same here - this needs to happen after the previous two 
+
+    // same here - this needs to happen after the previous two
     $.when(d1, d2).then(updateStatusInfo());
 }
 
