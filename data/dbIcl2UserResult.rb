7971da1e89085463991bce9fa7d8e450c0c1aa08
kalpathy <kalpathy@nmr.mgh.harvard.edu>
2015-05-02 14:13:15 -0400

    updated dbIc2UserResult to filter by icl, check for duplicates and save output

--- data/dbIcl2UserResult.rb
+++ data/dbIcl2UserResult.rb
@@ -1,11 +1,14 @@
 require 'json'
+require 'time'
 require 'csv'
 
-file = File.read('rop_images_4_27_2015.json')
+file = File.read('rop_images_1100_5_2_2015.json')
+#file = File.read('rop_images_4_27_2015.json')
+
 #file = File.read('/Users/jkc/Documents/retinalImaging/website/data/rop_images_4_27_2015.json')
 #file = File.read('/Users/jayashreekalpathy-cramer/Documents/rop/website/Image-Comparator/data/rop_images_4_27_2015.json')
 contents = JSON.parse(file)
-
+#puts contents
 iclDocs = contents['rows'].select{|x| x['doc']['type'] === "image_compare_list"}
 
 #puts iclDocs
@@ -34,25 +37,39 @@ tasks.each{|x| task2user[x['_id']] = x["user"]}
 #puts task2user.find {|key,val| val=="susan"}
 
 # now work on the results
+#puts contents.class
 icResultRows = contents['rows'].select{|x| x['doc']['type'] === "imageCompareResult"}
+#puts icResultRows.size
+#icResultRows2 = icResultRows.select{|x| task2icl[x['doc']['task']] === "icl_1_10_rev1"}
+icResultRows2 = icResultRows.select{|x| task2icl[x['doc']['task']] === "ICL_third_set_hundred_r2"}
+
+#puts icResultRows2.size
 
 icResults = []
-icResultRows.each{|x| icResults.push(x['doc'])}
+icResultRows2.each{|x| icResults.push(x['doc'])}
+#puts icResultRows2[0]
 
 # sort by task_idx
-icResults.sort!{|a,b| a['task_idx'] <=> b['task_idx']}
-#puts icResults.class
-
-#puts icResults[0]
+icResults.sort_by!{|a| [a['user'], a['task_idx'], Time.parse(a['date'])]}
 
 
+#icResults.sort!{|a,b| a['task_idx'] <=> b['task_idx']}
+puts icResults.size
+
+#puts icResults
+prevUser=''
+
+prevTaskIdx=''
 output = []
-output2=[]
 
-CSV.open("results3.csv", "w") do |csv|
+
+CSV.open("results_100_rev2.csv", "w") do |csv|
+  csv << ['task_id', 'image0', 'image1', 'winner', 'user', 'date', 'icl']
+#CSV.open("results_100_rev1.csv", "w") do |csv|
 icResults.each do |x|
   #puts "here"
   row = Hash.new()
+
   #row['task'] = x['task']
   row['task_idx'] = x['task_idx']
   row['image0'] = x['image0'] #.split('/').last
@@ -60,14 +77,33 @@ icResults.each do |x|
   row['winner'] = x['winner']
   row['user'] = x['user']
   row['date']=x['date']
+
+  thisUser= x['user']
+  thisTaskIdx=x['task_idx']
   thisTask=x['task']
   row['icl']=task2icl[thisTask]
+
   thisLine="#{x['task_idx']}, #{x['image0'].split('/').last}, #{x['image1'].split('/').last}, #{x['winner']}, #{x['user']}, #{x['date']}, #{row['icl']}"
-  puts thisLine
-  csv << [x['task_idx'], x['image0'].split('/').last, x['image1'].split('/').last, x['winner'], x['user'], x['date'], row['icl']]
-  output.push(row)
+  #puts thisLine
+  if(thisUser!=prevUser || thisTaskIdx!=prevTaskIdx)
+
+      csv << [x['task_idx'], x['image0'].split('/').last, x['image1'].split('/').last, x['winner'], x['user'], x['date'], row['icl']]
+      output.push(row)
+    else
+      puts thisUser
+      puts thisTask
+    
+  end
+  prevUser=thisUser
+  prevTaskIdx=thisTaskIdx
+
 end
 end
+
+puts output.size
+
+#b = output.group_by { |h| h['task_idx'] }.values.select { |a| a.size > 1 }.flatten
+#puts b.size
 
 #puts output.class
 
