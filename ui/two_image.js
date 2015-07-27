37a72c2ef16e133dbf15e9e810bcc3cceb1fca8e
Alan <alancharlescramer@gmail.com>
2015-07-27 10:14:47 -0400

    hide db and user dropdowns when search is not empty

--- ui/two_image.js
+++ ui/two_image.js
@@ -23,6 +23,19 @@ handleUrlFilter = function(urlSearchStr) {
         $("#username").val(user);
         OnSetUser(user);
     }
+    
+    // if urlSearchStr is not empty, remove the dropdown (db and user options)
+    if (urlSearchStr) {
+        var elem;
+        elem = document.getElementById("database");
+        elem.style.display='none'; // or ... style.visibility="hidden"; vis takes the same space, but is not shown
+        elem = document.getElementById("username");
+        elem.style.display='none';
+        
+        // also remove the Status info about the db
+        elem = document.getElementById("si_database");
+        elem.style.display='none';
+    }
 }
 
 
