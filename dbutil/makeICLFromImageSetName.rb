c11f6b24a5994b2b9d00537985c5301c244a553a
kalpathy <kalpathy@nmr.mgh.harvard.edu>
2016-10-30 20:55:29 -0400

    updated makeICLFromImageSetName

--- dbutil/makeICLFromImageSetName.rb
+++ dbutil/makeICLFromImageSetName.rb
@@ -20,7 +20,7 @@ pctRep =ARGV[1]
 nameStr = ARGV[2]
 
 # find range from searching db for images that have image_set_name
-viewUrl = "http://localhost:5984/rop_images/_design/basic_views/_view/imageSet2ImageId?key=\"#{imgSetName}\""
+viewUrl = "http://localhost:5984/rop_images/_design/basic_views/_view/imageSet2ImageId?key=\\\"#{imgSetName}\\\""
 uri = URI.parse(viewUrl)
 http = Net::HTTP.new(uri.host, uri.port)
 request = Net::HTTP::Get.new(uri.path)
